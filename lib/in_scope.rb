23bca4dfd8bbc6b4ce51602c56b8ede416332a1d
Chris Stadler <chrisstadler@gmail.com>
2024-05-12 10:56:13 -0400

    Extract into class

--- lib/in_scope.rb
+++ lib/in_scope.rb
@@ -1,49 +1,60 @@
 module InScope
   VERSION = "0.1.0"
 
-  def in_scope?(relation)
-    cores = relation.arel.ast.cores
-    if cores.length > 1
-      raise "More than one core"
-    end
-    root = cores.first
+  class Evaluator
+    def initialize(relation, instance)
+      @relation = relation
+      @instance = instance
 
-    objects_by_table = { self.class.table_name => self }
-    relation.values[:joins]&.each do |join_name|
-      object = self.public_send(join_name)
-      objects_by_table[object.class.table_name] = object
+      @instances_by_table = { instance.class.table_name => instance }
+      relation.values[:joins]&.each do |join_name|
+        object = instance.public_send(join_name)
+        @instances_by_table[object.class.table_name] = object
+      end
     end
 
-    # TODO: move out of method?
-    def eval_node(node, objects_by_table)
+    def eval
+      cores = @relation.arel.ast.cores
+      if cores.length > 1
+        raise "More than one core"
+      end
+      root = cores.first
+      eval_node(root)
+    end
+
+    private
+
+    def eval_node(node)
       case node
+      in Arel::Nodes::SelectCore
+        node.wheres.all? { |node| eval_node(node) }
       in Arel::Nodes::And
-        node.children.all? { |child| eval_node(child, objects_by_table) }
+        node.children.all? { |child| eval_node(child) }
       in Arel::Nodes::Or
-        eval_node(node.left, objects_by_table) || eval_node(node.right, objects_by_table)
+        eval_node(node.left) || eval_node(node.right)
       in Arel::Nodes::Equality
         attribute = node.left
         attribute_name = attribute.name
 
         table_name = attribute.relation.name
-        object = objects_by_table[table_name]
+        instance = @instances_by_table[table_name]
 
-        if !object
+        if !instance
           raise "Missing join for #{table_name}"
         end
 
         expected_value = node.right.value
 
-        object.public_send(attribute_name) == expected_value
+        instance.public_send(attribute_name) == expected_value
       in Arel::Nodes::Grouping
-        eval_node(node.expr, objects_by_table)
+        eval_node(node.expr)
       else
         raise "#{node.class} is an unsupported operation"
       end
     end
+  end
 
-    root.wheres.each_with_index.all? do |node, index|
-      eval_node(node, objects_by_table)
-    end
+  def in_scope?(relation)
+    Evaluator.new(relation, self).eval
   end
 end
